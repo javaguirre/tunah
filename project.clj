9b25e4d7274edaeef3199ed5014ccd0073d8cad2
Jyri Andrejev <jyriand@gmail.com>
2016-04-03 03:12:12 +0300

    Fix incorrect namespace in project.clj

--- project.clj
+++ project.clj
@@ -9,6 +9,6 @@
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [cfft "0.1.0"]
                  [net.mikera/vectorz-clj "0.33.0"]]
-  :main ^:skip-aot dsp.core
+  :main ^:skip-aot ctune.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
