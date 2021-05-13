daa26c2da63dfee8124ee0fc2899bf3c5960d3e7
Javier Aguirre <contacto@javaguirre.net>
2021-05-13 18:24:32 +0200

    Added clojure tone testing

--- test/tunah/core_test.clj
+++ test/tunah/core_test.clj
@@ -1,6 +1,5 @@
 (ns tunah.core-test
   (:require [clojure.test :refer :all]
-            [tunah.audio :refer :all]
             [tunah.core :refer :all]))
 
 (deftest cicd-test
@@ -9,8 +8,27 @@
 
 (deftest get-note-range-updates-max
   (testing "Max note range gets updated"
-      (let [frequency 600
-            note {:frequency 698 :note "f5" :common_name "F"}
-            note-range {:max-note {:frequency 1000 :note "XX" :common_name "X"}
-                        :min-note {:frequency 400 :note "XX" :common_name "X"}}]
-        (is (not= (get-note-range frequency note-range note) note-range)))))
+    (let [frequency 600
+          note {:frequency 698 :note "f5" :common_name "F"}
+          note-range {:max-note {:frequency 1000 :note "XX" :common_name "X"}
+                      :min-note {:frequency 400 :note "XX" :common_name "X"}}]
+      (is (not= (get-note-range frequency note-range note) note-range)))))
+
+(deftest get-closer-note-max-note-chosen
+  (testing "Max note gets chosen when a closer note is selected"
+    (let [frequency 600
+          max-note {:frequency 603 :note "XX" :common_name "X"}
+          note-range {:max-note max-note
+                      :min-note {:frequency 400 :note "XX" :common_name "X"}}]
+      (is (= (get-closer-note frequency note-range)
+             max-note)))))
+
+(deftest get-closer-note-min-note-chosen
+  (testing "Min note gets chosen when a closer note is selected"
+    (let [frequency 430
+          max-note {:frequency 603 :note "XX" :common_name "X"}
+          min-note {:frequency 400 :note "XX" :common_name "X"}
+          note-range {:max-note max-note
+                      :min-note min-note}]
+      (is (= (get-closer-note frequency note-range)
+             min-note)))))
